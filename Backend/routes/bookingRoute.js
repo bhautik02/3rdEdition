6262fdbfcaf4a9beac84effacb0c9385f64def18
bhautik <bhautikjani1102@gmail.com>
2023-06-22 15:46:10 +0530

    fetch user bug fixed

--- Backend/routes/bookingRoute.js
+++ Backend/routes/bookingRoute.js
@@ -5,7 +5,5 @@ const router = express.Router();
 
 router.route("/bookings").post(bookingController.bookPlace);
 router.route("/bookings/:id").get(bookingController.getBookings);
-// router
-//   .route("/create-payment-intent/:id")
-//   .post(bookingController.createPaymentIntent);
+
 module.exports = router;
